0d50a19e4a542e16f9e6e20b36b23fdb91b1a9ce
RohanJariwala <rohanweb777@gmail.com>
2023-01-07 08:56:55 +0530

    first commit

---
+++ js/Header.js
@@ -0,0 +1,45 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+    connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <div class="header_top">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="#">
+                        <img src="images/logo.png" alt="Home care management system" title="Home care management system"/>
+                    </a>
+                </div>
+                <div class="col-md-6 d-flex justify-content-end align-items-center">
+                    <a href="#" class="primary_btn">Login</a>
+                </div>
+            </div>
+        </div>
+        <nav id="nav_menu">
+          <ul class="d-flex flex-wrap justify-content-center">
+            <li><a href="../Home-Care-management-system/home.html" class="nav_links has_arrow active">Home</a></li>
+            <li><a href="#" class="nav_links">Clients</a></li>
+            <li><a href="#" class="nav_links">Providers</a></li>
+            <li><a href="#" class="nav_links has_arrow">Prior Authorizations</a></li>
+            <li><a href="#" class="nav_links has_arrow">Placements</a></li>
+            <li><a href="#" class="nav_links has_arrow">Accounting</a></li>
+            <li><a href="#" class="nav_links has_arrow">Billing</a></li>
+            <li><a href="#" class="nav_links has_arrow">Reports</a></li>
+            <li><a href="#" class="nav_links has_arrow">Setup</a></li>
+            <li><a href="#" class="nav_links has_arrow">Security</a></li>
+            <li><a href="#" class="nav_links has_arrow">Audit</a></li>
+            <li><a href="#" class="nav_links has_arrow">Feedback</a></li>
+          </ul>
+        </nav>
+        <div class="header_bottom d-flex justify-content-between">
+            <div class="date">Friday, December 9, 2022</div>
+            <div class="name"><a href="#" style="text-decoration:underline; color:var(--p_blue):">Mohammad Waleed</a></div>
+        </div>
+      </header>
+    `;
+  }
+}
+
+customElements.define("header-component", Header);
