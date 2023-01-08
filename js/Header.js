4e48734aaec41f685c722e1b3f294de0b99a0cf2
RohanJariwala <rohanweb777@gmail.com>
2023-01-08 08:55:57 +0530

    link changess

--- js/Header.js
+++ js/Header.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
         <div class="header_top">
             <div class="row">
                 <div class="col-md-6">
-                    <a href="../index.html">
+                    <a href="https://home-care-management-system.netlify.app/">
                         <img src="images/logo.png" alt="Home care management system" title="Home care management system"/>
                     </a>
                 </div>
@@ -19,8 +19,8 @@ class Header extends HTMLElement {
         </div>
         <nav id="nav_menu">
           <ul class="d-flex flex-wrap justify-content-center">
-            <li><a href="../index.html" class="nav_links has_arrow ">Home</a></li>
-            <li><a href="../clients.html" class="nav_links">Clients</a></li>
+            <li><a href="https://home-care-management-system.netlify.app" class="nav_links has_arrow ">Home</a></li>
+            <li><a href="https://home-care-management-system.netlify.app/clients.html" class="nav_links">Clients</a></li>
             <li><a href="../providers.html" class="nav_links">Providers</a></li>
             <li>
                 <a href="../prior-authorizations.html" class="nav_links has_arrow">Prior Authorizations</a>
