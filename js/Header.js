b7c6b77408a53cd3e25a8e9be3df362244f3c6f9
RohanJariwala <rohanweb777@gmail.com>
2023-01-08 09:34:31 +0530

    link update

--- js/Header.js
+++ js/Header.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
         <div class="header_top">
             <div class="row">
                 <div class="col-md-6">
-                    <a href="https://home-care-management-system.netlify.app/">
+                    <a href="../">
                         <img src="images/logo.png" alt="Home care management system" title="Home care management system"/>
                     </a>
                 </div>
@@ -19,22 +19,22 @@ class Header extends HTMLElement {
         </div>
         <nav id="nav_menu">
           <ul class="d-flex flex-wrap justify-content-center">
-            <li><a href="https://home-care-management-system.netlify.app" class="nav_links has_arrow ">Home</a></li>
-            <li><a href="https://home-care-management-system.netlify.app/clients.html" class="nav_links">Clients</a></li>
+            <li><a href="../" class="nav_links has_arrow ">Home</a></li>
+            <li><a href="../clients.html" class="nav_links">Clients</a></li>
             <li><a href="../providers.html" class="nav_links">Providers</a></li>
             <li>
                 <a href="../prior-authorizations.html" class="nav_links has_arrow">Prior Authorizations</a>
-                <div class="Inner_menu">
-                    <a href="#">Patient Assessments</a>
-                </div>
+                <ul class="Inner_menu">
+                  <li><a href="../patient-assessments.html">Patient Assessments</a></li>
+                </ul>
             </li>
             <li><a href="#" class="nav_links has_arrow">Placements</a></li>
             <li>
                 <a href="#" class="nav_links has_arrow">Accounting</a>
-                <div class="Inner_menu">
-                    <a href="#">Account Payable</a>
-                    <a href="#">Accounts Receivable</a>
-                </div>
+                <ul class="Inner_menu">
+                  <li><a href="#">Account Payable</a></li>
+                  <li><a href="#">Accounts Receivable</a></li>
+                </ul>
             </li>
             <li><a href="#" class="nav_links has_arrow">Billing</a></li>
             <li><a href="#" class="nav_links has_arrow">Reports</a></li>
