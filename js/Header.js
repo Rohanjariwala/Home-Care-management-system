3a964b9d4f3b7faac63e4adc515915f056cf4493
RohanJariwala <rohanweb777@gmail.com>
2023-01-17 21:14:30 +0530

    link changes and folders create

--- js/Header.js
+++ js/Header.js
@@ -19,50 +19,53 @@ class Header extends HTMLElement {
         </div>
         <nav id="nav_menu">
           <ul class="d-flex flex-wrap justify-content-center">
-            <li><a href="../Home-Care-management-system/" class="nav_links has_arrow ">Home</a></li>
-            <li><a href="../Home-Care-management-system/clients.html" class="nav_links">Clients</a></li>
-            <li><a href="../Home-Care-management-system/providers.html" class="nav_links">Providers</a></li>
+            <li><a href=".." class="nav_links has_arrow ">Home</a></li>
+            <li><a href="../clients.html" class="nav_links">Clients</a></li>
+            <li><a href="../providers.html" class="nav_links">Providers</a></li>
             <li>
                 <a href="../prior-authorizations.html" class="nav_links has_arrow">Prior Authorizations</a>
                 <ul class="Inner_menu">
-                  <li><a href="../patient-assessments.html">Patient Assessments</a></li>
+                  <li><a href="../prior-authorizations/patient-assessments.html">Patient Assessments</a></li>
                 </ul>
             </li>
             <li>
                 <a href="../placements.html" class="nav_links has_arrow">Placements</a>
                 <ul class="Inner_menu">
-                  <li><a href="../problem-providers.html">Problem Providers</a></li>
+                  <li><a href="../placements/problem-providers.html">Problem Providers</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#" class="nav_links has_arrow">Accounting</a>
                 <ul class="Inner_menu">
-                  <li><a href="../account-Payable.html">Account Payable</a></li>
-                  <li><a href="../account-receivable.html">Accounts Receivable</a></li>
+                  <li><a href="../accounting/account-Payable.html">Account Payable</a></li>
+                  <li><a href="../accounting/account-receivable.html">Accounts Receivable</a></li>
                 </ul>
             </li>
             <li>
                 <a href="../billing.html" class="nav_links has_arrow">Billing</a>
                 <ul class="Inner_menu">
-                  <li><a href="../billing-batch.html">Billing Batch</a></li>
-                  <li><a href="../billing-print-batch.html">Billing Print Batch</a></li>
+                  <li><a href="../billing/billing-batch.html">Billing Batch</a></li>
+                  <li><a href="../billing/billing-print-batch.html">Billing Print Batch</a></li>
                 </ul>
             </li>
             <li><a href="#" class="nav_links has_arrow">Reports</a>
                 <ul class="Inner_menu">
-                  <li><a href="../activity-report.html">Activity Report</a></li>
-                  <li><a href="../emergency-report.html">Emergency Report</a></li>
-                  <li><a href="../provider-schedule-update-report.html">Provider Schedule Update Report</a></li>
-                  <li><a href="../Missing-time-sheet-report.html">Missing Time Sheet Report</a></li>
-                  <li><a href="../time-sheet-report.html">Time Sheet Report</a></li>
-                  <li><a href="../potential-fraud-report.html">Potential Fraud Report</a></li>
-                  <li><a href="../pa-report.html">PA Report</a></li>
-                  <li><a href="../bulk-emails-report.html">Bulk Emails Report</a></li>
-                  <li><a href="../marketing-report.html">Marketing Report</a></li>
-                  <li><a href="../login-report.html">Login Report</a></li>
-                  <li><a href="../financial-report.html">Financial Report</a></li>
-                  <li><a href="../debts-report.html">Debts Report</a></li>
-                  <li><a href="../employee-summary-report.html">Employee Summary Report</a></li>
+                  <li><a href="../reports/activity-report.html">Activity Report</a></li>
+                  <li><a href="../reports/emergency-report.html">Emergency Report</a></li>
+                  <li><a href="../reports/provider-schedule-update-report.html">Provider Schedule Update Report</a></li>
+                  <li><a href="../reports/Missing-time-sheet-report.html">Missing Time Sheet Report</a></li>
+                  <li><a href="../reports/time-sheet-report.html">Time Sheet Report</a></li>
+                  <li><a href="../reports/potential-fraud-report.html">Potential Fraud Report</a></li>
+                  <li><a href="../reports/pa-report.html">PA Report</a></li>
+                  <li><a href="../reports/bulk-emails-report.html">Bulk Emails Report</a></li>
+                  <li><a href="../reports/marketing-report.html">Marketing Report</a></li>
+                  <li><a href="../reports/login-report.html">Login Report</a></li>
+                  <li><a href="../reports/financial-report.html">Financial Report</a></li>
+                  <li><a href="../reports/debts-report.html">Debts Report</a></li>
+                  <li><a href="../reports/employee-summary-report.html">Employee Summary Report</a></li>
+                  <li><a href="../reports/mobile-evv-report.html">Mobile EVV Report</a></li>
+                  <li><a href="../reports/evv-submission-report.html">EVV Submission Report</a></li>
+                  <li><a href="../reports/aging-report.html">Aging Report</a></li>
                 </ul>
             </li>
             <li><a href="#" class="nav_links has_arrow">Setup</a></li>
